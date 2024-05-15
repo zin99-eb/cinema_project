87078e542c6e2c9ed33c19b30473a0a049d560e7
Zineb Fakkar <Zineb.Fakkar@emsi-edu.ma>
2024-05-15 01:21:52 +0100

    4 com

--- src/main/java/fakkar/projetcinema/controllers/FilmController.java
+++ src/main/java/fakkar/projetcinema/controllers/FilmController.java
@@ -6,15 +6,11 @@ import fakkar.projetcinema.services.impl.TicketService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-
 public class FilmController {
     private FilmService filmService;
 
